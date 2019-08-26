55e84dc5f5e60a09324f57b79f89980d7e60681e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-26 16:50:34 -0400

    feat: connect dev database

--- src/firestore.js
+++ src/firestore.js
@@ -3,11 +3,21 @@ import Firestore from "@google-cloud/firestore";
 
 require("dotenv").config();
 
+let projectId = process.env.FIRESTORE_DEV_PROJECT_ID;
+let client_email = process.env.FIRESTORE_DEV_CLIENT_EMAIL;
+let private_key = process.env.FIRESTORE_DEV_PRIVATE_KEY;
+
+if (process.env.NODE_ENV === "production") {
+  projectId = process.env.FIRESTORE_PROJECT_ID;
+  client_email = process.env.FIRESTORE_CLIENT_EMAIL;
+  private_key = process.env.FIRESTORE_PRIVATE_KEY;
+}
+
 const firestore = new Firestore({
-  projectId: process.env.FIRESTORE_PROJECT_ID,
+  projectId,
   credentials: {
-    client_email: process.env.FIRESTORE_CLIENT_EMAIL,
-    private_key: process.env.FIRESTORE_PRIVATE_KEY
+    client_email,
+    private_key
   }
 });
 
