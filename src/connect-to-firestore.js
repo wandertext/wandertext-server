f84060f5b3142441bf8d097d59f126e5dc9d08e7
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-09-04 10:30:11 -0400

    refactor: Add wandertext-dev switch to connect script.

--- src/connect-to-firestore.js
+++ src/connect-to-firestore.js
@@ -2,12 +2,22 @@
 const Firestore = require("@google-cloud/firestore");
 require("dotenv").config();
 
+let projectId = process.env.FIRESTORE_DEV_PROJECT_ID;
+let client_email = process.env.FIRESTORE_DEV_CLIENT_EMAIL;
+let private_key = process.env.FIRESTORE_DEV_PRIVATE_KEY;
+
+if (process.env.NODE_ENV === "production") {
+  projectId = process.env.FIRESTORE_PROJECT_ID;
+  client_email = process.env.FIRESTORE_CLIENT_EMAIL;
+  private_key = process.env.FIRESTORE_PRIVATE_KEY;
+}
+
 module.exports = function() {
   return new Firestore({
-    projectId: process.env.FIRESTORE_PROJECT_ID,
+    projectId,
     credentials: {
-      private_key: process.env.FIRESTORE_PRIVATE_KEY,
-      client_email: process.env.FIRESTORE_CLIENT_EMAIL
+      client_email,
+      private_key
     }
   });
 };
