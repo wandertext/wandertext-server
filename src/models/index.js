d0610f91e3fb9550e2b16ca54fa05694dd307750
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-01-14 12:59:37 -0500

    feat: refactor model definition into an exported function.

--- src/models/index.js
+++ src/models/index.js
@@ -9,51 +9,53 @@ import entryModel from "./entry";
 import placeModel from "./place";
 import flagModel from "./flag";
 
-const env = process.env.NODE_ENV || "development";
-const config = configFile[env];
+export default function models() {
+  const env = process.env.NODE_ENV || "development";
+  const config = configFile[env];
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  let sequelize;
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  } else {
+    sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config
+    );
+  }
+
+  const Entry = entryModel(sequelize, Sequelize);
+  const Contributor = contributorModel(sequelize, Sequelize);
+  const Text = textModel(sequelize, Sequelize);
+  const Place = placeModel(sequelize, Sequelize);
+  const Flag = flagModel(sequelize, Sequelize);
+  const ContributorText = contributorTextModel(sequelize, Sequelize);
+  const ContributorEntry = contributorEntryModel(sequelize, Sequelize);
+  const ContributorPlace = contributorPlaceModel(sequelize, Sequelize);
+
+  Contributor.belongsToMany(Text, { through: ContributorText, unique: false });
+  Contributor.belongsToMany(Place, { through: ContributorPlace, unique: false });
+  Contributor.belongsToMany(Entry, { through: ContributorEntry, unique: false });
+  Entry.belongsTo(Text);
+  Entry.belongsTo(Place);
+  Entry.belongsToMany(Contributor, { through: ContributorEntry, unique: false });
+  Text.belongsToMany(Contributor, { through: ContributorText, unique: false });
+  Place.belongsToMany(Contributor, { through: ContributorPlace, unique: false });
+  Flag.belongsTo(Contributor);
+  Flag.belongsTo(Place);
+  Flag.belongsTo(Entry);
+  Flag.belongsTo(Text);
+
+  sequelize.sync().then(() => {
+    console.log(`Database & tables created!`);
+  });
+
+  return {
+    Entry,
+    Place,
+    Contributor,
+    Flag,
+    Text
+  };
 }
-
-const Entry = entryModel(sequelize, Sequelize);
-const Contributor = contributorModel(sequelize, Sequelize);
-const Text = textModel(sequelize, Sequelize);
-const Place = placeModel(sequelize, Sequelize);
-const Flag = flagModel(sequelize, Sequelize);
-const ContributorText = contributorTextModel(sequelize, Sequelize);
-const ContributorEntry = contributorEntryModel(sequelize, Sequelize);
-const ContributorPlace = contributorPlaceModel(sequelize, Sequelize);
-
-Contributor.belongsToMany(Text, { through: ContributorText, unique: false });
-Contributor.belongsToMany(Place, { through: ContributorPlace, unique: false });
-Contributor.belongsToMany(Entry, { through: ContributorEntry, unique: false });
-Entry.belongsTo(Text);
-Entry.belongsTo(Place);
-Entry.belongsToMany(Contributor, { through: ContributorEntry, unique: false });
-Text.belongsToMany(Contributor, { through: ContributorText, unique: false });
-Place.belongsToMany(Contributor, { through: ContributorPlace, unique: false });
-Flag.belongsTo(Contributor);
-Flag.belongsTo(Place);
-Flag.belongsTo(Entry);
-Flag.belongsTo(Text);
-
-sequelize.sync().then(() => {
-  console.log(`Database & tables created!`);
-});
-
-module.exports = {
-  Entry,
-  Place,
-  Contributor,
-  Flag,
-  Text
-};
