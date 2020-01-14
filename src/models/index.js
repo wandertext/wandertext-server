42295bd74b7282f7229c4b46709552aadf1f64fb
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-01-14 15:48:41 -0500

    feat: toggle logging on the actual model file, not in config.

--- src/models/index.js
+++ src/models/index.js
@@ -13,6 +13,8 @@ export default function models() {
   const env = process.env.NODE_ENV || "development";
   const config = configFile[env];
 
+  config.logging = false;
+
   let sequelize;
   if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
